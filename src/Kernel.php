c24c9aa627f6a61dffbad42d7cc75705174a20b7
Adrien Guillement <adrien.guillement@gmail.com>
2018-07-18 09:12:16 +0200

    Add use fixture in kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,6 +8,7 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class Kernel extends BaseKernel
 {
